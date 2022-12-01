610f5a9c52725f9012b020d87dcf71a3f10f8cfe
Bass <suddya@toptal.com>
2022-12-01 13:43:22 +0200

    remove immutable-state-invariant lib from import

--- src/redux/configureStore.ts
+++ src/redux/configureStore.ts
@@ -1,7 +1,6 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer, {persistConfig} from './reducers';
 import { persistStore, persistReducer } from 'redux-persist';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import createSagaMiddleware from 'redux-saga';
 import mySaga from '../sagas';
 
@@ -9,7 +8,7 @@ export default function configureStore(initialState: any) {
     const persistedReducer = persistReducer(persistConfig, rootReducer);
     const sagaMiddleware = createSagaMiddleware();
     const middleware = process.env.NODE_ENV !== 'production' ?
-    [reduxImmutableStateInvariant(), sagaMiddleware] :
+    [require('redux-immutable-state-invariant').default(), sagaMiddleware] :
     [sagaMiddleware];
     const store = createStore(
         persistedReducer,
